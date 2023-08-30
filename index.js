7f4cab0fbcb88aafd00bb2d6d5124442d249ea55
KASSYA BEATHRYS ROSA SILVA <kassya.silva@a.ucb.br>
2023-08-30 10:21:34 -0300

    atualizacao

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-const calc = require('.util/calculadora');
+const calc = require('./util/calculadora');
 
 console.log(calc.somar(1,2));
--- util/calculadora.js
+++ util/calculadora.js
@@ -1,4 +1,4 @@
-module.export = {
+module.exports = {
     somar: function(a,b){
         return a+b;
     },
