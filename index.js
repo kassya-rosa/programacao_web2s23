42bd695151cc7546be7aa2483ab833febfa79294
KASSYA BEATHRYS ROSA SILVA <kassya.silva@a.ucb.br>
2023-08-30 10:57:01 -0300

    atualizacao

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 const calc = require('./util/calculadora');
+const express = require('express');
+const app = express();
 
-console.log(calc.somar(1,2));
+app.get("/hello", function(req,res){
+    res.send("Olá");
+});
+
+app.get("/somar/:a/:b", function(req,res){ // o : faz a web enxergar o que o usuário está digitando na requisição
+    let a = req.params.a;
+    let b = req.params.b;
+    res.send(`${a} + ${b} = ${calc.somar(a,b)}`);
+});
+
+
+const port = 8080; // para poder rodar na web o express que foi baixado e a porta
+
+app.listen(port,function(){
+    console.log("app rodando na porta " +port);
+});
+
+//console.log(calc.somar(1,2));
--- util/calculadora.js
+++ util/calculadora.js
@@ -1,14 +1,14 @@
 module.exports = {
     somar: function(a,b){
-        return a+b;
+        return Number.parseInt(a)+Number.parseInt(b);
     },
     subtrair: function(a,b){
-        return a-b;
+        return Number.parseInt(a)-Number.parseInt(b);
     },
     multiplicar: function(a,b){
-        return a*b;
+        return Number.parseInt(a)*Number.parseInt(b);
     },
     dividir: function(a,b){
-        return a/b;
+        return Number.parseInt(a)/Number.parseInt(b);
     }
 }
