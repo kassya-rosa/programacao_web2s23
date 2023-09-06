0ee1c9e2c5b8feaa49c26d33c8b39c355789391f
KASSYA BEATHRYS ROSA SILVA <kassya.silva@a.ucb.br>
2023-09-06 09:40:39 -0300

    Atualizacao

--- index.js
+++ index.js
@@ -12,6 +12,24 @@ app.get("/somar/:a/:b", function(req,res){ // o : faz a web enxergar o que o usu
     res.send(`${a} + ${b} = ${calc.somar(a,b)}`);
 });
 
+app.get("/multiplicar/:a/:b", function(req,res){ // o : faz a web enxergar o que o usuário está digitando na requisição
+    let a = req.params.a;
+    let b = req.params.b;
+    res.send(`${a} * ${b} = ${calc.multiplicar(a,b)}`);
+});
+
+app.get("/dividir/:a/:b", function(req,res){ // o : faz a web enxergar o que o usuário está digitando na requisição
+    let a = req.params.a;
+    let b = req.params.b;
+    res.send(`${a} * ${b} = ${calc.dividir(a,b)}`);
+});
+
+app.get("/subtrair/:a/:b", function(req,res){ // o : faz a web enxergar o que o usuário está digitando na requisição
+    let a = req.params.a;
+    let b = req.params.b;
+    res.send(`${a} * ${b} = ${calc.subtrair(a,b)}`);
+});
+
 
 const port = 8080; // para poder rodar na web o express que foi baixado e a porta
 
